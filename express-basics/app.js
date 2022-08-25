5055271740f6f76c408fbba76985dddc97439e4d
maxrngl <lik4ever@hotmail.com>
2022-08-24 22:03:26 -0500

    added endpoints

--- express-basics/app.js
+++ express-basics/app.js
@@ -16,6 +16,35 @@ const posts = [
 // Init our Express app
 const app = express();
 
+// Enable Express app to receive JSON data
+app.use(express.json()); // Middleware
+
+// Define endpoints
+
+// Users endpoints
+app.get('/users', (req, res) => {
+	res.status(200).json({
+		status: 'success',
+		data: {
+			users,
+		},
+	});
+});
+
+app.post('/users', (req, res) => {
+	console.log(req.body);
+});
+
+// Posts endpoints
+app.get('/posts', (req, res) => {
+	res.status(200).json({
+		status: 'success',
+		data: {
+			posts,
+		},
+	});
+});
+
 // Set server to listen
 app.listen(4000, () => {
 	console.log('Express app running!');
