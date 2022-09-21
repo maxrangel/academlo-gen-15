e2125caa72e1559b289c50a8c6f8b94bd0352d64
maxrngl <lik4ever@hotmail.com>
2022-09-20 20:36:19 -0500

    created models relations

--- game-store-exercise/models/initModels.js
+++ game-store-exercise/models/initModels.js
@@ -1,6 +1,27 @@
 // Models
 const { User } = require('./user.model');
+const { Review } = require('./review.model');
+const { Game } = require('./game.model');
+const { Console } = require('./console.model');
 
-const initModels = () => {};
+const initModels = () => {
+	// 1 User <----> M Review
+	User.hasMany(Review, { foreignKey: 'userId' });
+	Review.belongsTo(User);
+
+	// 1 Game <----> M Review
+	Game.hasMany(Review, { foreignKey: 'gameId' });
+	Review.belongsTo(Game);
+
+	// M Game <----> M Console
+	Game.belongsToMany(Console, {
+		through: 'gameInConsole',
+		foreignKey: 'gameId',
+	});
+	Console.belongsToMany(Game, {
+		through: 'gameInConsole',
+		foreignKey: 'consoleId',
+	});
+};
 
 module.exports = { initModels };
