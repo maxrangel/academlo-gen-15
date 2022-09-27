688f8e9aeb2096ebe3c873e085f4ae9344fa6800
maxrngl <lik4ever@hotmail.com>
2022-09-26 21:59:17 -0500

    uploading and getting imgs from firebase

--- security/controllers/posts.controller.js
+++ security/controllers/posts.controller.js
@@ -1,9 +1,10 @@
-const { ref, uploadBytes } = require('firebase/storage');
+const { ref, uploadBytes, getDownloadURL } = require('firebase/storage');
 
 // Models
 const { Post } = require('../models/post.model');
 const { User } = require('../models/user.model');
 const { Comment } = require('../models/comment.model');
+const { PostImg } = require('../models/postImg.model');
 
 // Utils
 const { catchAsync } = require('../utils/catchAsync.util');
@@ -21,12 +22,19 @@ const getAllPosts = catchAsync(async (req, res, next) => {
 				where: { status: 'active' },
 				attributes: ['id', 'comment', 'status', 'createdAt'],
 			},
+			{
+				model: PostImg,
+			},
 		],
 	});
 
+	const imgRef = ref(storage, posts[0].postImgs[0].imgUrl);
+
+	const imgUrl = await getDownloadURL(imgRef);
+
 	res.status(200).json({
 		status: 'success',
-		data: { posts },
+		data: { posts, imgUrl },
 	});
 });
 
@@ -34,26 +42,29 @@ const createPost = catchAsync(async (req, res, next) => {
 	const { title, content } = req.body;
 	const { sessionUser } = req;
 
-	// const newPost = await Post.create({
-	// 	title,
-	// 	content,
-	// 	userId: sessionUser.id,
-	// });
+	const newPost = await Post.create({
+		title,
+		content,
+		userId: sessionUser.id,
+	});
 
 	// Create firebase reference
 	const [originalName, ext] = req.file.originalname.split('.'); // -> [pug, jpg]
 
-	const filename = `${originalName}-${Date.now()}.${ext}`;
+	const filename = `posts/${newPost.id}/${originalName}-${Date.now()}.${ext}`;
 	const imgRef = ref(storage, filename);
 
 	// Upload image to Firebase
 	const result = await uploadBytes(imgRef, req.file.buffer);
 
-	console.log(result);
+	await PostImg.create({
+		postId: newPost.id,
+		imgUrl: result.metadata.fullPath,
+	});
 
 	res.status(201).json({
 		status: 'success',
-		data: {},
+		data: { newPost },
 	});
 });
 
--- security/models/initModels.js
+++ security/models/initModels.js
@@ -2,6 +2,7 @@
 const { User } = require('./user.model');
 const { Post } = require('./post.model');
 const { Comment } = require('./comment.model');
+const { PostImg } = require('./postImg.model');
 
 const initModels = () => {
 	// 1 User <----> M Post
@@ -15,6 +16,10 @@ const initModels = () => {
 	// 1 User <----> M Comment
 	User.hasMany(Comment, { foreignKey: 'userId' });
 	Comment.belongsTo(User);
+
+	// 1 Post <----> M PostImg
+	Post.hasMany(PostImg, { foreignKey: 'postId' });
+	PostImg.belongsTo(Post);
 };
 
 module.exports = { initModels };
