c0579b3e5bb8169644cecffb91c1986333845f13
maxrngl <lik4ever@hotmail.com>
2022-09-27 21:47:27 -0500

    optimized storage functions

--- security/controllers/posts.controller.js
+++ security/controllers/posts.controller.js
@@ -1,5 +1,3 @@
-const { ref, uploadBytes, getDownloadURL } = require('firebase/storage');
-
 // Models
 const { Post } = require('../models/post.model');
 const { User } = require('../models/user.model');
@@ -8,7 +6,7 @@ const { PostImg } = require('../models/postImg.model');
 
 // Utils
 const { catchAsync } = require('../utils/catchAsync.util');
-const { storage } = require('../utils/firebase.util');
+const { uploadPostImgs, getPostsImgsUrls } = require('../utils/firebase.util');
 
 const getAllPosts = catchAsync(async (req, res, next) => {
 	const posts = await Post.findAll({
@@ -28,26 +26,7 @@ const getAllPosts = catchAsync(async (req, res, next) => {
 		],
 	});
 
-	// Loop through posts to get to the postImgs
-	const postsWithImgsPromises = posts.map(async post => {
-		// Get imgs URLs
-		const postImgsPromises = post.postImgs.map(async postImg => {
-			const imgRef = ref(storage, postImg.imgUrl);
-			const imgUrl = await getDownloadURL(imgRef);
-
-			postImg.imgUrl = imgUrl;
-			return postImg;
-		});
-
-		// Resolve imgs urls
-		const postImgs = await Promise.all(postImgsPromises);
-
-		// Update old postImgs array with new array
-		post.postImgs = postImgs;
-		return post;
-	});
-
-	const postsWithImgs = await Promise.all(postsWithImgsPromises);
+	const postsWithImgs = await getPostsImgsUrls(posts);
 
 	res.status(200).json({
 		status: 'success',
@@ -65,24 +44,7 @@ const createPost = catchAsync(async (req, res, next) => {
 		userId: sessionUser.id,
 	});
 
-	// Map async -> Async operations with arrays
-	const postImgsPromises = req.files.map(async file => {
-		// Create firebase reference
-		const [originalName, ext] = file.originalname.split('.'); // -> [pug, jpg]
-
-		const filename = `posts/${newPost.id}/${originalName}-${Date.now()}.${ext}`;
-		const imgRef = ref(storage, filename);
-
-		// Upload image to Firebase
-		const result = await uploadBytes(imgRef, file.buffer);
-
-		await PostImg.create({
-			postId: newPost.id,
-			imgUrl: result.metadata.fullPath,
-		});
-	});
-
-	await Promise.all(postImgsPromises);
+	await uploadPostImgs(req.files, newPost.id);
 
 	res.status(201).json({
 		status: 'success',
--- security/utils/firebase.util.js
+++ security/utils/firebase.util.js
@@ -1,5 +1,13 @@
 const { initializeApp } = require('firebase/app');
-const { getStorage } = require('firebase/storage');
+const {
+	getStorage,
+	ref,
+	uploadBytes,
+	getDownloadURL,
+} = require('firebase/storage');
+
+// Model
+const { PostImg } = require('../models/postImg.model');
 
 const dotenv = require('dotenv');
 
@@ -13,6 +21,52 @@ const firebaseConfig = {
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
+
+// Storage service
 const storage = getStorage(firebaseApp);
 
-module.exports = { storage };
+const uploadPostImgs = async (imgs, postId) => {
+	// Map async -> Async operations with arrays
+	const imgsPromises = imgs.map(async img => {
+		// Create firebase reference
+		const [originalName, ext] = img.originalname.split('.'); // -> [pug, jpg]
+
+		const filename = `posts/${postId}/${originalName}-${Date.now()}.${ext}`;
+		const imgRef = ref(storage, filename);
+
+		// Upload image to Firebase
+		const result = await uploadBytes(imgRef, img.buffer);
+
+		await PostImg.create({
+			postId,
+			imgUrl: result.metadata.fullPath,
+		});
+	});
+
+	await Promise.all(imgsPromises);
+};
+
+const getPostsImgsUrls = async posts => {
+	// Loop through posts to get to the postImgs
+	const postsWithImgsPromises = posts.map(async post => {
+		// Get imgs URLs
+		const postImgsPromises = post.postImgs.map(async postImg => {
+			const imgRef = ref(storage, postImg.imgUrl);
+			const imgUrl = await getDownloadURL(imgRef);
+
+			postImg.imgUrl = imgUrl;
+			return postImg;
+		});
+
+		// Resolve imgs urls
+		const postImgs = await Promise.all(postImgsPromises);
+
+		// Update old postImgs array with new array
+		post.postImgs = postImgs;
+		return post;
+	});
+
+	return await Promise.all(postsWithImgsPromises);
+};
+
+module.exports = { storage, uploadPostImgs, getPostsImgsUrls };
